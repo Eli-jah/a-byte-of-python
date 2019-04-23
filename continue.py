7fd3c714cf660f14e41b7446a824847c7ccbc4d8
elijah <1198331919@qq.com>
2019-04-23 11:15:21 +0800

    feat(control-flow): if, while, for, break, continue

---
+++ continue.py
@@ -0,0 +1,11 @@
+while True:
+    s = input('Enter something : ')
+    if s == 'quit':
+        print('quitting ...')
+        break
+    if len(s) < 3:
+        print('Too small')
+        continue
+    print('Input is of sufficient length: {0}'.format(len(s)))
+    # Do other kinds of processing here...
+print('Done')
---
+++ for.py
@@ -0,0 +1,6 @@
+for i in range(1, 7):
+    print(i)
+else:
+    print('The for-in loop is over')
+
+print('Done')
