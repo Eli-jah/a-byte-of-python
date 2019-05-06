1c3689b54c000c36cef5f47e4ec2bccdf94141cf
elijah <1198331919@qq.com>
2019-05-06 15:50:44 +0800

    feat(module): basic usage of module sys and customized module

---
+++ module_using_sys.py
@@ -0,0 +1,11 @@
+import sys
+from my_module import say_hi, __version__
+
+print('The command arguments are:')
+for i in sys.argv:
+    print(i)
+
+print('\nThe PYTHONPATH is:', sys.path, '\n')
+
+say_hi()
+print('Version:', __version__)
---
+++ my_module.py
@@ -0,0 +1,5 @@
+def say_hi():
+    print('Hello, this is my module.')
+
+
+__version__ = '0.1'
